9306c55376325058c06f2ee64ddbea31c6234ada
InsarMag <insar.magadeev@gmail.com>
2021-11-27 15:42:11 +0500

    Initial commit

---
+++ review.js
@@ -0,0 +1,29 @@
+if(5 > 3){
+    // код
+}
+else if(5 <= 3) {
+
+}
+else if(2 == 3) {
+
+}
+
+if(true){
+
+}
+else {
+
+}
+
+// циклы
+var counter = 0;
+while(counter < 10) {
+    counter = counter + 22
+    // counter += 4;
+}
+
+for(var x = 0; x < 5; x += 2) {
+    
+}
+
+var foo = [13, "s;akj", 'd', 235]
